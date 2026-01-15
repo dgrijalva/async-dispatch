d196a8a1fce707c3169ed9f2fbf9a552911b0258
Dave Grijalva <grijalva@gmail.com>
2026-01-15 10:26:26 -0800

    Add documentation for v0.2.1

---
+++ v0.2.1/trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["async_dispatch",[["impl UnsafeUnpin for <a class=\"struct\" href=\"async_dispatch/struct.Sleep.html\" title=\"struct async_dispatch::Sleep\">Sleep</a>",1,["async_dispatch::Sleep"]],["impl&lt;T&gt; UnsafeUnpin for <a class=\"struct\" href=\"async_dispatch/struct.Task.html\" title=\"struct async_dispatch::Task\">Task</a>&lt;T&gt;",1,["async_dispatch::Task"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[362]}
---
+++ v0.2.1/trait.impl/core/panic/unwind_safe/trait.UnwindSafe.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["async_dispatch",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/panic/unwind_safe/trait.UnwindSafe.html\" title=\"trait core::panic::unwind_safe::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"async_dispatch/struct.Sleep.html\" title=\"struct async_dispatch::Sleep\">Sleep</a>",1,["async_dispatch::Sleep"]],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/panic/unwind_safe/trait.UnwindSafe.html\" title=\"trait core::panic::unwind_safe::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"async_dispatch/struct.Task.html\" title=\"struct async_dispatch::Task\">Task</a>&lt;T&gt;",1,["async_dispatch::Task"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[686]}
